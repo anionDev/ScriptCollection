c5f8e93bd6f237297d8a75faba8ff5aa6eeb5c08
Marius Göcke <marius.goecke@gmail.com>
2019-05-26 18:03:00 +0200

    added changehashofprogram

---
+++ scripts/filemanipulation/ChangeHashOfProgram.py
@@ -0,0 +1,25 @@
+from shutil import copy2
+import argparse
+
+parser = argparse.ArgumentParser(description='Changes the hash-value of arbitrary files by appending a whitespace at the end of the file.')
+
+parser.add_argument('inputFile', type=str, help='Specifies the script/executable-file whose hash-value should be changed')
+
+parser.add_argument('--valueToAppend', type=str, default=" ", help = 'Specifies the string should should be appended to the file')
+parser.add_argument('--outputFile', type=str, default="", help = 'Specifies the outputfile and its location')
+
+args = parser.parse_args()
+
+inputFile = args.inputFile.replace("\"","")
+valueToAppend= args.valueToAppend
+outputFile = args.outputFile
+
+if(outputFile==""):
+    outputFile=inputFile+".modified"
+
+copy2(inputFile, outputFile)
+file = open(outputFile, 'a')
+file.write(valueToAppend)
+file.close()
+
+
