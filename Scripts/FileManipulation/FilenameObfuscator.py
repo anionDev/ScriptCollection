871539d4f0448a70f7ef2bd4a06edce74720e72e
Marius Göcke <marius.goecke@gmail.com>
2019-05-26 22:38:53 +0200

    improved filenameobfuscator

--- Scripts/FileManipulation/FilenameObfuscator.py
+++ Scripts/FileManipulation/FilenameObfuscator.py
@@ -8,16 +8,36 @@ import sys
 parser = argparse.ArgumentParser(description='Obfuscates the names of all files in the given folder. Caution: This script can cause harm if you pass a wrong inputFolder-argument')
 
 parser.add_argument('inputFolder', type=str, help='Specifies the foldere where the files are stored whose names should be obfuscated')
-parser.add_argument('--nameMappingFile', type=str, default="NameMapping.txt", help = 'Specifies the file where the name-mapping will be written to')
+parser.add_argument('--nameMappingFile', type=str, default="NameMapping.csv", help = 'Specifies the file where the name-mapping will be written to')
+parser.add_argument('--printTableHeadline', default = True, help='Prints the first')
 
 args = parser.parse_args()
 
+def toBoolean(value):
+    if(type(value) is bool):
+        return value
+    if value.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif value.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError(value + ' is not boolean value.')
 def get_sha256_of_file(file:str):
     sha256 = hashlib.sha256()
     with open(file, "rb") as fileObject:
         for chunk in iter(lambda: fileObject.read(4096), b""):
             sha256.update(chunk)
     return sha256.hexdigest()
+
+def append_line_to_file(file, line_content):
+    with open(file, "a") as fileObject:
+        mapping_file_is_empty = os.stat(file).st_size == 0
+        if mapping_file_is_empty:
+            new_line=""
+        else:
+            new_line="\n"
+        fileObject.write(new_line + line_content)
+
 def remove_extension(filename, extension):
   if extension == "":
     return filename
@@ -28,27 +48,26 @@ def remove_extension(filename, extension):
 
 directory=os.fsdecode(os.fsencode(args.inputFolder))
 if (os.path.isdir(directory)):
+    printTableHeadline=toBoolean(args.printTableHeadline)
     files = []
     if not os.path.isfile(args.nameMappingFile):
-        with open(args.nameMappingFile, "w"):
+        with open(args.nameMappingFile, "a"):
             pass
-    mapping_file_is_empty=os.stat(args.nameMappingFile).st_size == 0
+    with open(args.nameMappingFile, "a") as fileObject:
+        pass
+    if printTableHeadline:
+        append_line_to_file(args.nameMappingFile, "Original filename;new filename;SHA2-hash of file")
     for file in os.listdir(directory):
         if os.path.isfile(os.path.join(directory, file)):
             files.append(file)
-    with open(args.nameMappingFile, "a") as fileObject:
-        for file in files:
-            full_file_name=os.path.join(directory, file)
-            hash=get_sha256_of_file(full_file_name)
-            if mapping_file_is_empty:
-              new_line=""
-            else:
-              new_line="\n"
-            extension=pathlib.Path(file).suffix
-            file_without_extension=remove_extension(file, extension)
-            new_file_name=os.path.join(directory, str(uuid.uuid4()) + extension)
-            os.rename(full_file_name, new_file_name)
-            fileObject.write(new_line + full_file_name + ";" + new_file_name + ";" + hash)
-            mapping_file_is_empty=False
+    for file in files:
+        full_file_name=os.path.join(directory, file)
+        hash=get_sha256_of_file(full_file_name)
+        extension=pathlib.Path(file).suffix
+        file_without_extension=remove_extension(file, extension)
+        new_file_name=os.path.join(directory, str(uuid.uuid4()) + extension)
+        os.rename(full_file_name, new_file_name)
+        append_line_to_file(args.nameMappingFile, full_file_name + ";" + new_file_name + ";" + hash)
+        mapping_file_is_empty=False
 else:
     sys.exit(2)
