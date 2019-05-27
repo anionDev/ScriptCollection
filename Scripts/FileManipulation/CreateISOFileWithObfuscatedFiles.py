a5a533cd8d2a0e086194275a6ebbd23ae633270e
Marius Göcke <marius.goecke@gmail.com>
2019-05-27 12:33:57 +0200

    update

--- Scripts/FileManipulation/CreateISOFileWithObfuscatedFiles.py
+++ Scripts/FileManipulation/CreateISOFileWithObfuscatedFiles.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import subprocess
-
+from shutil import copy2
 parser = argparse.ArgumentParser(description='Creates an iso file with the files in the given folder and changes their names and hash-values.')
 
 parser.add_argument('inputFolder', type=str, help='Specifies the foldere where the files are stored which should be added to the iso-file')
@@ -9,9 +9,22 @@ parser.add_argument('--outputFile', type=str, default="files.iso", help = 'Speci
 
 args = parser.parse_args()
 
+def get_files_in_directory(directory):
+    files = []
+    for file in os.listdir(directory):
+        file_with_directory=os.path.join(directory, file)
+        if os.path.isfile(file_with_directory):
+            files.append(file_with_directory)
+    return files
+
 directory=os.fsdecode(os.fsencode(args.inputFolder))
 if (os.path.isdir(directory)):
-    pass#TODO
+    temp_directory=directory+"_temp"
+    if not os.path.exists(temp_directory):
+        os.makedirs(temp_directory)
+    for file in get_files_in_directory(directory):
+        copy2(file, temp_directory)
+    #TODO
 else:
     print('Directory not found: ' + directory)
     sys.exit(2)
