7f82e7e007e5e88a94f8121f3f43082e9433ed44
Marius Göcke <marius.goecke@gmail.com>
2019-06-17 22:02:46 +0200

    replaced tabs by whitespaces

--- Scripts/Build/BuildDotNetProgram/RestoreNugetPackages.py
+++ Scripts/Build/BuildDotNetProgram/RestoreNugetPackages.py
@@ -10,16 +10,16 @@ start_directory=os.getcwd()
 os.chdir(os.path.dirname(os.path.basename(__file__)))
 try:
 
-	parser = argparse.ArgumentParser(description='Downloads missing nuget-packages')
-	parser.add_argument('--inputfolder', help='Specifies the folder of the packages.config')
+    parser = argparse.ArgumentParser(description='Downloads missing nuget-packages')
+    parser.add_argument('--inputfolder', help='Specifies the folder of the packages.config')
 
-	args = parser.parse_args()
-	##TODO do this only if packages.config-file exists in projectfolder
-	exitCode=utilities.execute("nuget", "restore -SolutionDirectory ..", args.inputfolder)
-	if exitCode!=0:
-		sys.exit(exitCode)
+    args = parser.parse_args()
+    ##TODO do this only if packages.config-file exists in projectfolder
+    exitCode=utilities.execute("nuget", "restore -SolutionDirectory ..", args.inputfolder)
+    if exitCode!=0:
+        sys.exit(exitCode)
 
-	print("Finished " + os.path.basename(__file__) + " without errors")
+    print("Finished " + os.path.basename(__file__) + " without errors")
     sys.exit(0)
 
 finally:
