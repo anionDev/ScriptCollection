6033a58257c612a8af027d61e5a3be4b27f65f76
Marius Göcke <marius.goecke@gmail.com>
2019-06-26 11:53:55 +0200

    some implementation for searchinfiles

--- Scripts/Miscellaneous/SearchInFiles.py
+++ Scripts/Miscellaneous/SearchInFiles.py
@@ -12,20 +12,23 @@ parser.add_argument('searchstring', help = 'string to look for')
 
 args = parser.parse_args()
 
-bytes_ascii=0#TODO
-bytes_unicode=0#TODO
-
+bytes_ascii = bytes(args.searchstring,"ascii")
+bytes_utf16 = bytes(args.searchstring,"utf-16")#often called "unicode-encoding"
 def absolute_file_paths(directory:str):
    for dirpath,_,filenames in os.walk(directory):
        for f in filenames:
            yield os.path.abspath(os.path.join(dirpath, f))
 
 def file_contains_byte_array(file:str, byte_array:str):
-    return false#TODO
+    try:
+        with open(file, mode='rb') as file:
+           return byte_array in file.read()
+    except:
+        pass
 def check_file(file:str):
-    if file_contains_string(file,bytes_ascii):
+    if file_contains_byte_array(file,bytes_ascii):
         print(file)
-    if file_contains_string(file,bytes_unicode):
+    elif file_contains_byte_array(file,bytes_utf16):
         print(file)
 
 for file in absolute_file_paths(args.folder):
