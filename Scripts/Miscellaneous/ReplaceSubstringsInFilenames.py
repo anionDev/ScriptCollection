021ef866491693701402f8754b2a92cf427316d4
Marius Göcke <marius.goecke@gmail.com>
2019-07-29 13:35:24 +0200

    updated replacesubstringsinfilename

--- Scripts/Miscellaneous/ReplaceSubstringsInFilenames.py
+++ Scripts/Miscellaneous/ReplaceSubstringsInFilenames.py
@@ -6,13 +6,12 @@ This program requires "pip install Send2Trash".
 import argparse
 import os
 import filecmp
-
 parser = argparse.ArgumentParser(description='Replaces certain substrings in filenames. This program requires "pip install Send2Trash" in certain cases.')
 
 parser.add_argument('folder', help='Folder where the files are stored which should be renamed')
 parser.add_argument('substringInFilename', help='String to be replaced')
 parser.add_argument('newSubstringInFilename', help='new string value for filename')
-parser.add_argument('conflictResolveMode', help='Set a method how to handle cases where a file with the new filename already exits and the files have not the same content. Possible values are: ignore, preservenewest')
+parser.add_argument('conflictResolveMode', help='Set a method how to handle cases where a file with the new filename already exits and the files have not the same content. Possible values are: ignore, preservenewest, merge')
 
 args = parser.parse_args()
 
@@ -22,8 +21,17 @@ def absolute_file_paths(directory:str):
            yield os.path.abspath(os.path.join(dirpath, filename))
 
 def move_to_bin(file:str):
-    from send2trash import send2trash
-    send2trash(file)
+    import send2trash
+    send2trash.send2trash(file)
+
+merge_separator=[0x0A]
+def merge_files(sourcefile:str, targetfile:str):
+    with open(sourcefile, "rb") as f:
+        source_data = f.read()
+    fout = open(targetfile, "ab")
+    fout.write(bytes(merge_separator))
+    fout.write(source_data)
+    fout.close()
 
 def process_file(file:str):
     new_filename=os.path.join(os.path.dirname(file),os.path.basename(file).replace(args.substringInFilename, args.newSubstringInFilename))
@@ -34,12 +42,15 @@ def process_file(file:str):
             else:
                 if(args.conflictResolveMode=="ignore"):
                     pass
-                if(args.conflictResolveMode=="preservenewest"):
+                elif(args.conflictResolveMode=="preservenewest"):
                     if(os.path.getmtime(file) - os.path.getmtime(new_filename) > 0):
                         move_to_bin(file)
                     else:
                         move_to_bin(new_filename)
                         os.rename(file, new_filename)
+                elif(args.conflictResolveMode=="merge"):
+                    merge_files(file, new_filename)
+                    move_to_bin(file)
                 else:
                     raise Exception('Unknown conflict resolve mode')
         else:
