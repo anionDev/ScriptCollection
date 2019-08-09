314ae245e1ebb9d4d80dab8952389f0360c9ff55
Marius Göcke <marius.goecke@gmail.com>
2019-08-09 22:16:58 +0200

    added general utilities.py

--- Scripts/Miscellaneous/CreateHashOfAllFiles.py
+++ Scripts/Miscellaneous/CreateHashOfAllFiles.py
@@ -3,8 +3,7 @@ Tested on: Windows
 This program comes with absolutely no warranty.
 """
 import argparse
-import os
-import hashlib
+import Utilities
 
 parser = argparse.ArgumentParser(description='Calculates the SHA-256-value of all files in the given folder and stores the hash-value in a file next to the hashed file.')
 
@@ -12,18 +11,6 @@ parser.add_argument('folder', help='Folder where the files are stored which shou
 
 args = parser.parse_args()
 
-def absolute_file_paths(directory:str):
-   for dirpath,_,filenames in os.walk(directory):
-       for f in filenames:
-           yield os.path.abspath(os.path.join(dirpath, f))
-
-def get_sha256_of_file(file:str):
-    sha256 = hashlib.sha256()
-    with open(file, "rb") as fileObject:
-        for chunk in iter(lambda: fileObject.read(4096), b""):
-            sha256.update(chunk)
-    return sha256.hexdigest()
-
-for file in absolute_file_paths(args.folder):
+for file in Utilities.absolute_file_paths(args.folder):
     with open(file+".sha256","w+") as f:
-        f.write(get_sha256_of_file(file))
+        f.write(Utilities.get_sha256_of_file(file))
---
+++ Scripts/Miscellaneous/Utilities.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+
+def absolute_file_paths(directory:str):
+   for dirpath,_,filenames in os.walk(directory):
+       for f in filenames:
+           yield os.path.abspath(os.path.join(dirpath, f))
+
+def get_sha256_of_file(file:str):
+    sha256 = hashlib.sha256()
+    with open(file, "rb") as fileObject:
+        for chunk in iter(lambda: fileObject.read(4096), b""):
+            sha256.update(chunk)
+    return sha256.hexdigest()
+
+def execute(program:str, arguments:str, workingdirectory:str="",timeout=120):
+    if not os.path.isabs(workingdirectory):
+        workingdirectory=os.path.abspath(workingdirectory)
+    #print("Start '"+workingdirectory +" "+program + " " + arguments+"'")
+    exit_code = subprocess.call(program + " " + arguments, cwd=workingdirectory, timeout=timeout)
+    #print("Finished '"+workingdirectory +" "+program + " " + arguments +"' with exitcode "+str(exit_code))
+    return exit_code
