daa50e86140dbb2ddbd3fc877e20f706904fe224
Marius Göcke <marius.goecke@gmail.com>
2019-09-08 18:48:10 +0200

    added exit-code-checks for git-functions in powershell

--- Scripts/Git/CommonGitFunctions.ps1
+++ Scripts/Git/CommonGitFunctions.ps1
@@ -36,13 +36,11 @@ function ShowUnexpectedChangesInGitRepository($repositoryFolder, $expectedChange
             $p.WaitForExit()
             $exitCode=$p.ExitCode
             if($exitCode -ne 0){
-                Write-Host "'Git diff $_' had exitcode $exitCode"
-                Exit $exitCode
+                throw [System.Exception] "'Git diff $_' had exitcode $exitCode"
             }
             $stdout = $p.StandardOutput.ReadToEnd()
             Write-Host "Unexpected change in file '$_' :"
             Write-Host $stdout
-
         }
     }
     if($repositoryHasUnexpectedChanges){
@@ -55,7 +53,24 @@ function GetAllRemotes($repositoryFolder){
     $originalWorkingDirectory=(Get-Item -Path ".\").FullName
     try{
         cd $repositoryFolder
-        $remotes = (git remote) | Out-String
+        
+        
+        
+        $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+        $pinfo.FileName = "git"
+        $pinfo.RedirectStandardError = $true
+        $pinfo.RedirectStandardOutput = $true
+        $pinfo.WorkingDirectory=$sourceFolder
+        $pinfo.UseShellExecute = $false
+        $pinfo.Arguments = "remote"
+        $p = New-Object System.Diagnostics.Process
+        $p.StartInfo = $pinfo
+        $p.Start() | Out-Null
+        $p.WaitForExit()
+        $exitCode=$p.ExitCode
+        if($exitCode -ne 0){
+            throw [System.Exception] "'Git remote' had exitcode $exitCode"
+        }
         return $remotes.split([Environment]::NewLine)
     }
     finally{
@@ -67,7 +82,21 @@ function RemoveAllRemotes($repositoryFolder){
     try{
         cd $repositoryFolder
         ForEach($remote in GetAllRemotes($repositoryFolder)){
-            git remote remove origin $remote
+            $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+            $pinfo.FileName = "git"
+            $pinfo.RedirectStandardError = $true
+            $pinfo.RedirectStandardOutput = $true
+            $pinfo.WorkingDirectory=$sourceFolder
+            $pinfo.UseShellExecute = $false
+            $pinfo.Arguments = "remote remove $remote"
+            $p = New-Object System.Diagnostics.Process
+            $p.StartInfo = $pinfo
+            $p.Start() | Out-Null
+            $p.WaitForExit()
+            $exitCode=$p.ExitCode
+            if($exitCode -ne 0){
+                throw [System.Exception] "'Git remote remove $remote' had exitcode $exitCode"
+            }
         }
     }
     finally{
