a1e9c643b3e1845390a8dcbde65bfcf3a32e651d
Marius Göcke <marius.goecke@gmail.com>
2019-09-11 17:02:19 +0200

    moved commit to correct file

--- Scripts/Git/CommonGitFunctions.py
+++ Scripts/Git/CommonGitFunctions.py
@@ -1,4 +1,10 @@
+from Utilities import *
+
 def repository_has_uncommitted_changes(repository_folder:str):
     pass#TODO
 def get_current_commit_id(repository_folder:str):
     pass#TODO return the output of "git rev-parse --verify HEAD"
+
+def commit(directory:str, message:str):
+    execute("git","add -A", directory, 3600)
+    execute("git","commit -m \""+message+"\"",directory)
