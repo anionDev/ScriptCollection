cdc04d4a164e24c93c16d37ca9a0c9514d06c2f6
Marius Göcke <marius.goecke@gmail.com>
2019-09-11 16:52:14 +0200

    added some placeholder

---
+++ Scripts/Git/CommonGitFunctions.py
@@ -0,0 +1,4 @@
+def repository_has_uncommitted_changes(repository_folder:str):
+    pass#TODO
+def get_current_commit_id(repository_folder:str):
+    pass#TODO return the output of "git rev-parse --verify HEAD"
