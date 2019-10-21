5e36440888ea73f131d4e2594fad2cf64c81e3fc
Marius Göcke <marius.goecke@gmail.com>
2019-10-21 18:03:28 +0200

    refactoring

--- Scripts/Build/BuildDotNetProgram/BuildProject.py
+++ Scripts/Build/BuildDotNetProgram/BuildProject.py
@@ -10,6 +10,7 @@ from os.path import abspath
 import shutil
 sys.path.append(abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),f"..{os.path.sep}..{os.path.sep}Miscellaneous")))
 from Utilities import *
+
 write_message_to_stdout("Start " + os.path.basename(__file__))
 start_directory=os.getcwd()
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -17,12 +18,21 @@ try:
     parser = argparse.ArgumentParser(description='Compiles a csproj-file. This scripts also download required nuget-packages.')
     parser.add_argument('--folder_of_csproj_filename', help='Specifies the folder where the csproj-file is located')
     parser.add_argument('--csproj_filename', help='Specifies the csproj-file which should be compiled')
-    parser.add_argument('--configuration', help='Specifies the Buildconfiguration (e.g. Debug or Release)')
+    parser.add_argument('--buildconfiguration', help='Specifies the Buildconfiguration (e.g. Debug or Release)')
     parser.add_argument('--additional_msbuild_arguments', help='Specifies arbitrary arguments which are passed to msbuild')
     parser.add_argument('--output_directory', help='Specifies output directory for the compiled program')
     parser.add_argument('--clear_output_directory', type=string_to_boolean, nargs='?', const=True, default=False,help='If true then the output directory will be cleared before compiling the program')
     args = parser.parse_args()
-    output_folder=os.path.abspath(os.path.join(os.path.abspath(args.folder_of_csproj_filename),"bin\\" + args.configuration))
+    
+    write_message_to_stdout(f"Arguments:")
+    write_message_to_stdout("folder_of_csproj_filename: " + args.folder_of_csproj_filename)
+    write_message_to_stdout("csproj_filename: " + args.csproj_filename)
+    write_message_to_stdout("buildconfiguration: " + args.buildconfiguration)
+    write_message_to_stdout("additional_msbuild_arguments: " + args.additional_msbuild_arguments)
+    write_message_to_stdout("output_directory: " + args.output_directory)
+    write_message_to_stdout("clear_output_directory: " + str(args.clear_output_directory))
+    sys.exit(0)
+    output_folder=os.path.abspath(os.path.join(os.path.abspath(args.folder_of_csproj_filename),"bin\\" + args.buildconfiguration))
     exit_code=execute("python", "RestoreNugetPackages.py --inputfolder "+args.folder_of_csproj_filename, os.getcwd())
     if exit_code!=0:
         sys.exit(exit_code)
@@ -30,7 +40,7 @@ try:
         shutil.rmtree(output_folder)
     os.makedirs(output_folder)
 
-    exit_code=execute("msbuild", args.csproj_filename+" /t:Rebuild /verbosity:normal /p:Configuration="+args.configuration+" /p:Platform=AnyCPU /p:OutputPath="+output_folder+" "+str_none_safe(args.additional_msbuild_arguments), args.folder_of_csproj_filename)
+    exit_code=execute("msbuild", args.csproj_filename+" /t:Rebuild /verbosity:normal /p:Configuration="+args.buildconfiguration+" /p:Platform=AnyCPU /p:OutputPath="+output_folder+" "+str_none_safe(args.additional_msbuild_arguments), args.folder_of_csproj_filename)
     if exit_code!=0:
         write_message_to_stderr("msbuild had exitcode " +str(exit_code))
         sys.exit(exit_code)
--- Scripts/Build/BuildDotNetProgram/ExecuteTestcases.py
+++ Scripts/Build/BuildDotNetProgram/ExecuteTestcases.py
@@ -2,6 +2,10 @@ import argparse
 import os
 import sys
 import shutil
+from os.path import abspath
+sys.path.append(abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),f"..{os.path.sep}..{os.path.sep}Miscellaneous")))
+from Utilities import *
+
 write_message_to_stdout("Start " + os.path.basename(__file__))
 start_directory=os.getcwd()
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -11,9 +15,9 @@ try:
     parser.add_argument('--testdll', help='Specifies the dll-file where the testcases are stored in')
     args = parser.parse_args()
 
-    print("TODO")
+    write_message_to_stdout(f"Arguments:")
+    write_message_to_stdout("testdll: " + args.testdll)
 
-    sys.stdout.write("Finished " + os.path.basename(__file__) + " without errors\n")
-    sys.stdout.flush()
+    write_message_to_stdout("Finished " + os.path.basename(__file__) + " without errors")
 finally:
     os.chdir(start_directory)
