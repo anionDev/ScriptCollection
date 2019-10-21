6b9021b8a9aec10128230ab0641b15a49e0e6424
Marius Göcke <marius.goecke@gmail.com>
2019-10-21 15:40:45 +0200

    made dolutiondir- customizable in nuget-restore-script

--- Scripts/Build/BuildDotNetProgram/RestoreNugetPackages.py
+++ Scripts/Build/BuildDotNetProgram/RestoreNugetPackages.py
@@ -11,12 +11,13 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 try:
     parser = argparse.ArgumentParser(description='Downloads missing nuget-packages')
-    parser.add_argument('--inputfolder', help='Specifies the folder of the packages.config')
+    parser.add_argument('--inputfolder', help='Specifies the folder of the .csproj-file')
+    parser.add_argument('--solutiondirectory', default="..", help='Specifies the folder of the .sln-file')
 
     args = parser.parse_args()
     packages_config_file=os.path.join(args.inputfolder,"packages.config")
     if(os.path.isfile(packages_config_file)):
-        exit_code=execute("nuget", "restore -SolutionDirectory ..", args.inputfolder)
+        exit_code=execute("nuget", "restore -SolutionDirectory "+args.solutiondirectory, args.inputfolder)
         if exit_code!=0:
             sys.stderr.write("nuget had exitcode " +str(exit_code)+"\n")
             sys.exit(exit_code)
