0187bf825d9da85428d3fd36c52a4fc624e33107
Marius Göcke <marius.goecke@gmail.com>
2019-11-11 09:44:51 +0100

    finished implementation of createvm-script

--- Scripts/VMWare/CreateVirtualMachine.py
+++ Scripts/VMWare/CreateVirtualMachine.py
@@ -1,11 +1,13 @@
 """
 Tested on: Windows
+This script requires that the command 'qemu-img' is available on your system.
 This program comes with absolutely no warranty.
 """
 import argparse
 import shutil
 import os
 import sys
+import io
 sys.path.append(f"..{os.path.sep}Miscellaneous")
 from Utilities import *
 
@@ -13,6 +15,7 @@ parser = argparse.ArgumentParser(description='Creates a new virtual-machine as v
 parser.add_argument('name', type=str,help='Name of virtual machine')
 parser.add_argument('outputfolder',type=str,help='Folder where the virtual machine should be saved')
 parser.add_argument('--memsize', help='RAM-size in MB', default="4096")
+parser.add_argument('--disksize', help='disk-size in GB', default="20")
 parser.add_argument('--guestOS', help='Type of guest-vm. Example: debian10-64', default="debian10-64")
 parser.add_argument('--coresPerSocket', default="2")
 args = parser.parse_args()
@@ -25,20 +28,22 @@ outputfolder=resolve_relative_path_from_current_working_directory(args.outputfol
 vmx_file=args.name+".vmx"
 vmx_file_with_path=os.path.join(outputfolder,vmx_file)
 
-def replace_underscore_in_string(string:str,replace_from:str, replace_to:str):
-    return string.replace("__"+replace_from+"__",replace_to)
+def replace_underscore_in_string(s:str,replace_from:str, replace_to:str):
+    return s.replace("__"+replace_from+"__",replace_to)
 
 ensure_directory_exists(outputfolder)
-shutil.copy(template_vmx,outputfolder)
-
-vmx_content=""#todo read file
-vmx_content=replace_underscore_in_string(vmx_content,"name",args.name)
-vmx_content=replace_underscore_in_string(vmx_content,"outputfolder",args.outputfolder)
-vmx_content=replace_underscore_in_string(vmx_content,"memsize",args.memsize)
-vmx_content=replace_underscore_in_string(vmx_content,"guestOS",args.guestOS)
-#todo write vmx_content back to file
-
+ensure_file_exists(vmx_file_with_path)
 diskname=args.name+"Disk"
-#todo create vmdk-file with qemu
 
+with io.open(template_vmx, 'r', encoding="utf-8") as template_file:
+    vmx_content=template_file.read()
+    vmx_content=replace_underscore_in_string(vmx_content,"name", args.name)
+    vmx_content=replace_underscore_in_string(vmx_content,"coresPerSocket", args.coresPerSocket)
+    vmx_content=replace_underscore_in_string(vmx_content,"memsize", args.memsize)
+    vmx_content=replace_underscore_in_string(vmx_content,"guestOS", args.guestOS)
+    vmx_content=replace_underscore_in_string(vmx_content,"diskname", diskname)
+    with io.open(vmx_file_with_path, 'w+', encoding="utf-8") as target_file:
+        target_file.write(vmx_content)
+
+execute("qemu-img", f"create -f vmdk {diskname}.vmdk {args.disksize}G", outputfolder)
 
