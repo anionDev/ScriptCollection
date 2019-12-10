f5133719f7609fb2f938ac52db997ec74c1e4e4b
Marius Göcke <marius.goecke@gmail.com>
2019-12-10 15:48:11 +0100

    update for GenerateOverviewGraphics - simple graphics can now be generated

--- Scripts/Miscellaneous/GenerateOverviewGraphics.py
+++ Scripts/Miscellaneous/GenerateOverviewGraphics.py
@@ -14,6 +14,7 @@ args = parser.parse_args()
 
 generator=type('', (), {})()
 generator.core=type('', (), {})()
+generator.metadata=type('', (), {})()
 generator.gui=type('', (), {})()
 generator.gui.icons=[]
 generator.gui.formatting=[]
@@ -32,5 +33,14 @@ templates_folder=os.path.join(this_folder,"GenerateOverviewGraphicsHelper","Temp
 global_template_file=os.path.join(templates_folder,"Global.svg.template")
 keyvaluepairlist_template_file=os.path.join(templates_folder,"KeyValuePairListTemplate.svg.template")
 list_template_file=os.path.join(templates_folder,"List.svg.template")
-copyfile(global_template_file,generator.core.output)
+from pprint import pprint
+
+def calculate_overview():
+    with open(global_template_file, encoding='utf8') as global_template_file_object:
+        result=global_template_file_object.read()
+    result=result.replace("__Title__", f"{generator.metadata.title} ({generator.metadata.version})")
+    #todo
+    return result
+with open(generator.core.output, "r+", encoding='utf8') as result_file_object:
+    result_file_object.write(calculate_overview())
 
