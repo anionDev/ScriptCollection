a44b5f100e37f03097fda51e3eb288b994bec76e
Marius Göcke <marius.goecke@gmail.com>
2019-12-30 21:25:12 +0100

    fix in vmware-scripts

--- Scripts/VMWare/Utilities.py
+++ Scripts/VMWare/Utilities.py
@@ -1,12 +1,16 @@
-
+import sys
+import os
+sys.path.append(f"..{os.path.sep}Miscellaneous")
+from Utilities import *
 
 def execute_command_in_vm_and_expect_exitcode_zero(name_of_vm:str, directory:str, command:str):
     exitcode=execute_command_in_vm(name_of_vm,directory,command)
     if(exitcode!=0):
-        raise Exception(f"{name_of_vm}: '{directory}>{command}' had exitcode {str(exitcode)}"):
-def execute_command_in_vm(name_of_vm:str, directory:str, command:str)
-    if(not vm_is_running(name_of_vm):
-        raise Exception(f"{name_of_vm}: Can not execute '{directory}>{command}' because the vm is not running"):
+        raise Exception(f"{name_of_vm}: '{directory}>{command}' had exitcode {str(exitcode)}")
+
+def execute_command_in_vm(name_of_vm:str, directory:str, command:str):
+    if(not vm_is_running(name_of_vm)):
+        raise Exception(f"{name_of_vm}: Can not execute '{directory}>{command}' because the vm is not running")
         
     pass#todo execute and return exit-code
 
