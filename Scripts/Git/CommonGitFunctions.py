a343d13321110a0dc280b22e20a353e43ee1e530
Marius Göcke <marius.goecke@gmail.com>
2020-03-09 00:44:31 +0100

    some fixes in CommonGitFunctions

--- Scripts/Git/CommonGitFunctions.py
+++ Scripts/Git/CommonGitFunctions.py
@@ -5,17 +5,26 @@ sys.path.append(str(pathlib.Path(str(pathlib.Path(__file__).parent.absolute())+o
 from Utilities import *
 
 def repository_has_uncommitted_changes(repository_folder:str):
-    exit_code= execute("git","diff --exit-code --quiet", repository_folder)
+    argument="diff --exit-code --quiet"
+    exit_code = execute_and_raise_exception_if_exit_code_is_not_zero("git",argument, repository_folder)[0]
     if exit_code==0:
         return False
     if exit_code==1:
         return True
-    raise ValueError("'git diff --exit-code --quiet' results in exitcode "+str(exitcode))
+    raise ValueError(f"'git {argument}' results in exitcode "+str(exitcode))
 
 def get_current_commit_id(repository_folder:str):
-    result=execute_get_output_by_argument_array("git",["rev-parse", "--verify HEAD"], repository_folder)
+    argument="rev-parse --verify HEAD"
+    result=execute_and_raise_exception_if_exit_code_is_not_zero("git",argument, repository_folder)
     if not (result[0]==0):
-        raise ValueError("'git rev-parse --verify HEAD' results in exitcode "+str(exitcode))
+        raise ValueError(f"'git {argument}' results in exitcode "+str(exitcode))
+    return result[1].replace('\r','').replace('\n','')
+
+def push(folder:str, remotename:str, localbranchname:str, remotebranchname:str):
+    argument=f"push {remotename} {localbranchname}:{remotebranchname}"
+    result=execute_and_raise_exception_if_exit_code_is_not_zero("git",argument, repository_folder)
+    if not (result[0]==0):
+        raise ValueError(f"'git {argument}' results in exitcode "+str(exitcode))
     return result[1].replace('\r','').replace('\n','')
 
 def clone_if_not_already_done(folder:str, link:str):
@@ -23,25 +32,29 @@ def clone_if_not_already_done(folder:str, link:str):
     original_cwd=os.getcwd()
     try:
        if(not os.path.isdir(folder)):
-           execute_exit_code=execute("git", f"clone {link} --recurse-submodules --remote-submodules", original_cwd)
+           argument=f"clone {link} --recurse-submodules --remote-submodules"
+           execute_exit_code=execute_and_raise_exception_if_exit_code_is_not_zero(f"git {argument}", argument, original_cwd)[0]
            if execute_exit_code!=0:
-               print("Git clone had exitcode "+str(execute_exit_code))
+               print(f"'git {argument}' had exitcode "+str(execute_exit_code))
                exit_code=execute_exit_code
     finally:
         os.chdir(original_cwd)
     return exit_code
 
 def commit(directory:str, message:str):
-    exitcode=execute("git","add -A", directory, 3600)
+    argument="add -A"
+    exitcode=execute_and_raise_exception_if_exit_code_is_not_zero("git",argument, directory, 3600)[0]
     if not (exitcode==0):
-        raise ValueError("'git add' results in exitcode "+str(exitcode))
+        raise ValueError(f"'git {argument}' results in exitcode "+str(exitcode))
 
-    exitcode=execute_get_output_by_argument_array("git",["commit","-m \""+message+"\""], directory, 3600)[0]
+    argument=f"commit -m {message}"
+    exitcode=execute_and_raise_exception_if_exit_code_is_not_zero("git",argument, directory, 600)[0]
     if not (exitcode==0):
-        print("Warning: 'git commit' results in exitcode "+str(exitcode)+". This means that probably either there were no changes to commit or an error occurred while commiting")
+        print(f"Warning: 'git {argument}' results in exitcode "+str(exitcode)+". This means that probably either there were no changes to commit or an error occurred while commiting")
 
-    result = execute_get_output_by_argument_array("git", ["log","--format=\"%H\"","-n 1"], directory)
+    argument=f'log --format="%H" -n 1'
+    result = execute_and_raise_exception_if_exit_code_is_not_zero("git", argument, directory)[0]
     if not (result[0]==0):
-        raise ValueError("'git log' results in exitcode "+str(result[0]))
+        raise ValueError(f"'git {argument}' results in exitcode "+str(result[0]))
 
     return result[1].replace('\r','').replace('\n','')
