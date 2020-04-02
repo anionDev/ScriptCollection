52e0ea29914e7795919f26a0c6be9c7fde68df26
Marius Göcke <marius.goecke@gmail.com>
2020-04-02 20:38:07 +0200

    fix

--- Scripts/Git/CommonGitFunctions.py
+++ Scripts/Git/CommonGitFunctions.py
@@ -12,7 +12,7 @@ def repository_has_unstaged_changes(repository_folder:str):
         return True
     if(repository_has_new_untracked_files(repository_folder)):
         return True
-    return false
+    return False
 
 def repository_has_staged_changes(repository_folder:str):
     return repository_has_uncommitted_changes_helper(repository_folder,"diff --cached")  
