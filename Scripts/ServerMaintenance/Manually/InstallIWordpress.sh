fe19c65dfe0917a39c3854d6a035f4d23f310812
Marius Göcke <marius.goecke@gmail.com>
2020-04-28 13:27:31 +0200

    removed ansible-attempts

--- Scripts/ServerMaintenance/Manually/InstallIWordpress.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /var/www/html
-wget http://wordpress.org/latest.tar.gz
-tar xfz latest.tar.gz
-rm latest.tar.gz
-chown sudo chown -R www-data:www-data /wordpress
--- Scripts/ServerMaintenance/Manually/InstallPHPWithWordpressWithRequirements.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-./preparation.sh
-./apache.sh
-./mariadb.sh
-./php.sh
-./phpmyadmin.sh
-./wordpress.sh
