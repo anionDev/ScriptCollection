093f6c979df35e3b95f6d0a51495a9705216337b
Marius Göcke <marius.goecke@gmail.com>
2020-04-28 22:53:41 +0200

    added calling docfx to 04_reference

--- Scripts/Build/BuildDotNetProgram/CreateReleaseTasks/04_Reference.py
+++ Scripts/Build/BuildDotNetProgram/CreateReleaseTasks/04_Reference.py
@@ -38,13 +38,16 @@ try:
     publishdirectory=get_publishdirectory(configparser,version)
     opencoverreportfile=publishdirectory+os.path.sep+configparser.get('general','productname')+".TestCoverage.opencover.xml"
     
-    print(opencoverreportfile)
     execute_and_raise_exception_if_exit_code_is_not_zero("reportgenerator", '-reports:"'+opencoverreportfile+'" -targetdir:"'+configparser.get('reference','coveragereportfolder')+'"')
-    #todo
+    
+    docfx_file_with_path=configparser.get('reference','docfxfile')
+    docfxfolder=os.path.dirname(docfx_file_with_path)
+    docfxfile=os.path.basename(docfx_file_with_path)
+    execute_and_raise_exception_if_exit_code_is_not_zero("docfx", docfxfile, docfxfolder)
     
     commitmessage="Updated reference"
-    #commit(configparser.get('reference','referencerepository'), commitmessage)
-    #commit(configparser.get('release','releaserepository'), commitmessage)
+    commit(configparser.get('reference','referencerepository'), commitmessage)
+    commit(configparser.get('release','releaserepository'), commitmessage)
      
 except Exception as exception:
     write_exception_to_stderr_with_traceback(exception, traceback)
