1db7121bd5b268539df8f899cbf8127257e1254a
Marius Göcke <marius.goecke@gmail.com>
2020-05-28 20:43:26 +0200

    some refactoring

--- Scripts/miscellaneous/SCGenerateThumbnail.py
+++ Scripts/miscellaneous/SCGenerateThumbnail.py
@@ -7,17 +7,11 @@ Commands which must be available in the path-variable:
 -montage (from ImageMagick)
 -epew
 """
+
 import argparse
 import uuid
 from pathlib import Path
-from Utilities import *
 import datetime
-
-parser = argparse.ArgumentParser(description='Generate thumpnails for video-files')
-parser.add_argument('file', help='Input-videofile for thumbnail-generation')
-
-args = parser.parse_args()
-tempname_for_thumbnails="t"+str(uuid.uuid4())
 
 def calculate_lengh_in_seconds(file:str,wd:str):
     argument='-v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "'+file+'"'
@@ -34,17 +28,28 @@ def create_thumbnail(outputfilename:str,wd:str,length_in_seconds:float):
     argument='-title "'+outputfilename+" ("+info+')" -geometry +4+4 '+tempname_for_thumbnails+'*.png "'+outputfilename+'.png"'
     execute_and_raise_exception_if_exit_code_is_not_zero("montage",argument,wd)
 
-amount_of_images=16
-file=args.file
-filename=os.path.basename(file)
-folder=os.path.dirname(file)
-filename_without_extension=Path(file).stem
+def SCGenerateThumbnail_cli():
+    parser = argparse.ArgumentParser(description='Generate thumpnails for video-files')
+    parser.add_argument('file', help='Input-videofile for thumbnail-generation')
 
-try:
-    length_in_seconds=calculate_lengh_in_seconds(filename,folder)
-    create_thumbnails(filename,length_in_seconds,amount_of_images,folder)
-    create_thumbnail(filename_without_extension,folder,length_in_seconds)
-finally:
-    for thumbnail_to_delete in Path(folder).rglob(tempname_for_thumbnails+"-*"):
-        file=str(thumbnail_to_delete)
-        os.remove(file)
+    args = parser.parse_args()
+    tempname_for_thumbnails="t"+str(uuid.uuid4())
+
+
+    amount_of_images=16
+    file=args.file
+    filename=os.path.basename(file)
+    folder=os.path.dirname(file)
+    filename_without_extension=Path(file).stem
+
+    try:
+        length_in_seconds=calculate_lengh_in_seconds(filename,folder)
+        create_thumbnails(filename,length_in_seconds,amount_of_images,folder)
+        create_thumbnail(filename_without_extension,folder,length_in_seconds)
+    finally:
+        for thumbnail_to_delete in Path(folder).rglob(tempname_for_thumbnails+"-*"):
+            file=str(thumbnail_to_delete)
+            os.remove(file)
+
+if __name__ == '__main__':
+    SCGenerateThumbnail_cli()
--- Scripts/miscellaneous/SCOrganizeLinesInFile.py
+++ Scripts/miscellaneous/SCOrganizeLinesInFile.py
@@ -5,69 +5,72 @@ This program comes with absolutely no warranty.
 import argparse
 import os
 import sys
-from Utilities import *
 
-parser = argparse.ArgumentParser(description='Processes the lines of a file with the given commands')
+def SCOrganizeLinesInFile_cli():
+    parser = argparse.ArgumentParser(description='Processes the lines of a file with the given commands')
 
-parser.add_argument('file', help='File which should be transformed')
-parser.add_argument('--encoding', default="utf-8", help='Encoding for the file which should be transformed')
-parser.add_argument("--sort", type=string_to_boolean, nargs='?', const=True, default=False, help="Sort lines")
-parser.add_argument("--remove_duplicated_lines", type=string_to_boolean, nargs='?', const=True, default=False, help="Remove duplicate lines")
-parser.add_argument("--ignore_first_line", type=string_to_boolean, nargs='?', const=True, default=False, help="Ignores the first line in the file")
-parser.add_argument("--remove_empty_lines", type=string_to_boolean, nargs='?', const=True, default=False, help="Removes lines which are empty or contains only whitespaces")
+    parser.add_argument('file', help='File which should be transformed')
+    parser.add_argument('--encoding', default="utf-8", help='Encoding for the file which should be transformed')
+    parser.add_argument("--sort", type=string_to_boolean, nargs='?', const=True, default=False, help="Sort lines")
+    parser.add_argument("--remove_duplicated_lines", type=string_to_boolean, nargs='?', const=True, default=False, help="Remove duplicate lines")
+    parser.add_argument("--ignore_first_line", type=string_to_boolean, nargs='?', const=True, default=False, help="Ignores the first line in the file")
+    parser.add_argument("--remove_empty_lines", type=string_to_boolean, nargs='?', const=True, default=False, help="Removes lines which are empty or contains only whitespaces")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if os.path.isfile(args.file):
+    if os.path.isfile(args.file):
 
-    #read file
-    with open(args.file, 'r', encoding=args.encoding) as f:
-        content=f.read()
-    lines=content.splitlines()
-    
-    #remove trailing newlines
-    temp=[]
-    for line in lines:
-        temp.append(line.rstrip())
-    lines=temp
-    
-    #store first line if desired
-    if(len(lines) > 0 and args.ignore_first_line):
-        first_line=lines.pop(0)
+        #read file
+        with open(args.file, 'r', encoding=args.encoding) as f:
+            content=f.read()
+        lines=content.splitlines()
+        
+        #remove trailing newlines
+        temp=[]
+        for line in lines:
+            temp.append(line.rstrip())
+        lines=temp
+        
+        #store first line if desired
+        if(len(lines) > 0 and args.ignore_first_line):
+            first_line=lines.pop(0)
 
-    #remove empty lines if desired
-    if args.remove_empty_lines and False:
-        temp=lines
-        lines=[]
-        for line in temp:
-            if(not (string_is_none_or_whitespace(line))):
-                lines.append(line)
+        #remove empty lines if desired
+        if args.remove_empty_lines and False:
+            temp=lines
+            lines=[]
+            for line in temp:
+                if(not (string_is_none_or_whitespace(line))):
+                    lines.append(line)
 
-    #remove duplicated lines if desired
-    if args.remove_duplicated_lines:
-        lines=remove_duplicates(lines)
+        #remove duplicated lines if desired
+        if args.remove_duplicated_lines:
+            lines=remove_duplicates(lines)
 
-    #sort lines if desired
-    if args.sort:
-        lines=sorted(lines, key=str.casefold)
+        #sort lines if desired
+        if args.sort:
+            lines=sorted(lines, key=str.casefold)
 
-    #reinsert first line
-    if args.ignore_first_line:
-        lines.insert(0, first_line)
+        #reinsert first line
+        if args.ignore_first_line:
+            lines.insert(0, first_line)
 
-    #concat lines separated by newline
-    result=""
-    is_first_line=True
-    for line in lines:
-        if(is_first_line):
-            result=line
-            is_first_line=False
-        else:
-            result=result+'\n'+line
+        #concat lines separated by newline
+        result=""
+        is_first_line=True
+        for line in lines:
+            if(is_first_line):
+                result=line
+                is_first_line=False
+            else:
+                result=result+'\n'+line
 
-    #write result to file
-    with open(args.file, 'w', encoding=args.encoding) as f:
-        f.write(result)
-else:
-    print(f"File '{args.file}' does not exist")
-    sys.exit(1)
+        #write result to file
+        with open(args.file, 'w', encoding=args.encoding) as f:
+            f.write(result)
+    else:
+        print(f"File '{args.file}' does not exist")
+        sys.exit(1)
+
+if __name__ == '__main__':
+    SCOrganizeLinesInFile_cli()
