260a8b783994fedd6367d2b6fd745b92d6a6f5d7
Marius Göcke <marius.goecke@gmail.com>
2020-07-15 19:11:45 +0200

    Updated version to 1.0.8

--- Scripts/python/Setup.py
+++ Scripts/python/Setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-version = "1.0.4"
+version = "1.0.8"
 
 productname = "scriptCollection"
 packages= [package for package in find_packages() if not package.endswith("Tests")]
