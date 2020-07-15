36651d6b0d162a45c9abc2783697b03c84bf59d4
Marius Göcke <marius.goecke@gmail.com>
2020-07-15 21:37:59 +0200

    Updated version to 1.2.0

--- Scripts/python/Setup.py
+++ Scripts/python/Setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-version = "1.1.3"
+version = "1.2.0"
 
 productname = "ScriptCollection"
 packages= [package for package in find_packages() if not package.endswith("Tests")]
