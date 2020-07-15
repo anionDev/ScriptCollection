5b1cfd94b604ed39e41eaf6e4275b26fb489a3e3
Marius Göcke <marius.goecke@gmail.com>
2020-07-15 20:49:59 +0200

    added todo

--- Scripts/python/ScriptCollectionTests/tests.py
+++ Scripts/python/ScriptCollectionTests/tests.py
@@ -3,4 +3,4 @@ from ScriptCollection.core import get_scriptcollection_version
 
 
 def test_version():
-    assert get_scriptcollection_version().startswith("1.0.")
+    assert get_scriptcollection_version().startswith("1.0.")#TODO
