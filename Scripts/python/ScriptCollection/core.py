cde858e36e7882a90f56b83b24fce6ea2439d6bc
Marius Göcke <marius.goecke@gmail.com>
2020-07-26 14:23:18 +0200

    Updated version to 1.3.2

--- Scripts/python/ScriptCollection/core.py
+++ Scripts/python/ScriptCollection/core.py
@@ -29,7 +29,7 @@ import traceback
 from os.path import isfile, join, isdir
 from os import listdir
 import datetime
-version = "1.3.1"
+version = "1.3.2"
 
 
 # <Build>
--- Scripts/python/Setup.py
+++ Scripts/python/Setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-version = "1.3.1"
+version = "1.3.2"
 
 productname = "ScriptCollection"
 packages= [package for package in find_packages() if not package.endswith("Tests")]
