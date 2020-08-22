7bac5dbb3cde45f9c1eb9b338953bfc33ebeeffc
Marius Göcke <marius.goecke@gmail.com>
2020-08-22 11:49:20 +0200

    (re)moved some old functions

--- Scripts/other/Miscellaneous/GenerateOverviewGraphicsHelper/Examples/SimpleDemo/BasicExampleConfiguration.py
+++
@@ -1,6 +0,0 @@
-generator.core.datasource="Data.json"
-generator.core.output="Result.svg"
-generator.metadata.title="MyTool"
-generator.metadata.version="1.0.0"
-generator.gui.formatting.append(["data.Name","Headline"])
-generator.gui.icons.append(["data.Repositories","Repository.svg"])
