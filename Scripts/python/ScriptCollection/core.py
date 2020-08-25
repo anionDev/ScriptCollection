695b001c0a5408ea1a8f4897042f2a3cc102def6
Marius Göcke <marius.goecke@gmail.com>
2020-08-26 00:32:08 +0200

    updates in SCDotNetReference

--- Scripts/python/ScriptCollection/core.py
+++ Scripts/python/ScriptCollection/core.py
@@ -322,10 +322,11 @@ def SCDotNetReference(configurationfile: str):
     if configparser.getboolean('dotnet', 'generatereference'):
         docfx_file = get_buildscript_config_item(configparser, 'dotnet', 'docfxfile')
         docfx_filefolder = os.path.dirname(docfx_file)
-        _private_replace_underscore_in_file_for_buildconfiguration(get_buildscript_config_item(configparser, 'dotnet', 'referencerepositoryindexfile'), configparser)
         execute_and_raise_exception_if_exit_code_is_not_zero("docfx", docfx_file, docfx_filefolder)
-        shutil.copyfile(_private_get_test_csprojfile_folder(configparser)+os.path.sep+_private_get_coverage_filename(configparser), get_buildscript_config_item(configparser, 'dotnet', 'coveragefolder')+os.path.sep+_private_get_coverage_filename(configparser))
-        execute_and_raise_exception_if_exit_code_is_not_zero("reportgenerator", '-reports:"'+_private_get_coverage_filename(configparser)+'" -targetdir:"'+get_buildscript_config_item(configparser, 'dotnet', 'coveragereportfolder')+'"', get_buildscript_config_item(configparser, 'dotnet', 'coveragefolder'))
+        coveragefolder=get_buildscript_config_item(configparser, 'dotnet', 'coveragefolder')
+        coveragefile=+os.path.sep+_private_get_coverage_filename(configparser)
+        shutil.copyfile(_private_get_test_csprojfile_folder(configparser)+os.path.sep+_private_get_coverage_filename(configparser), coveragefile)
+        execute_and_raise_exception_if_exit_code_is_not_zero("reportgenerator", '-reports:"'+_private_get_coverage_filename(configparser)+'" -targetdir:"'+coveragefolder+'"',coveragefolder)
         git_commit(get_buildscript_config_item(configparser, 'dotnet', 'referencerepository'), "Updated reference")
         if configparser.getboolean('dotnet', 'exportreference'):
             git_push(get_buildscript_config_item(configparser, 'dotnet', 'referencerepository'), get_buildscript_config_item(configparser, 'dotnet', 'exportreferenceremotename'), "master", "master", False, False)
@@ -711,7 +712,6 @@ def _private_replace_underscores_for_buildconfiguration(string: str, configparse
     available_configuration_items.append(["dotnet", "docfxfile"])
     available_configuration_items.append(["dotnet", "coveragefolder"])
     available_configuration_items.append(["dotnet", "coveragereportfolder"])
-    available_configuration_items.append(["dotnet", "referencerepositoryindexfile"])
     available_configuration_items.append(["dotnet", "referencerepository"])
     available_configuration_items.append(["dotnet", "exportreferenceremotename"])
     available_configuration_items.append(["dotnet", "publishtargetrepository"])
