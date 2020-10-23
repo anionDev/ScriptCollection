2d3decf4b0404c99d6d86565923a484f208855cd
Marius Göcke <marius.goecke@gmail.com>
2020-10-23 19:14:09 +0200

    updated setup.py to make it more portable

--- Scripts/Python/Setup.py
+++ Scripts/Python/Setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 import os
 from pathlib import Path
 
+productname = "ScriptCollection"
 version = "1.12.24"
 
-productname = "ScriptCollection"
 packages = [package for package in find_packages() if not package.endswith("Tests")]
 
 folder_of_current_file=os.path.dirname(os.path.realpath(__file__))
-with open(str(Path(os.path.join(folder_of_current_file, "..\\..\\ReadMe.md")).resolve()), "r", encoding='utf-8') as f:
+with open(str(Path(os.path.join(folder_of_current_file, f"..{os.sep}..{os.sep}ReadMe.md")).resolve()), "r", encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
