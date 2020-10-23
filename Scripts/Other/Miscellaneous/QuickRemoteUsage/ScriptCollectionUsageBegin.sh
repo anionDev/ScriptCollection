8c750161432c38a2fe7ffe50b28a171ecd7a14b9
Marius Göcke <marius.goecke@gmail.com>
2020-10-24 00:20:16 +0200

    updated script

--- Scripts/Other/Miscellaneous/QuickRemoteUsage/ScriptCollectionUsageBegin.sh
+++ Scripts/Other/Miscellaneous/QuickRemoteUsage/ScriptCollectionUsageBegin.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-apt update
-apt -y install git
+apt-get update
+apt-get -y install git
 git clone https://github.com/anionDev/ScriptCollection.git
-chmod chown -R user:user ./ScriptCollection
