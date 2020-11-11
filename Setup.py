d3ec5b18d4992640d8a3ffe07b0b601d9cb079a3
Marius Göcke <marius.goecke@gmail.com>
2020-11-11 19:34:03 +0100

    little simplification

--- Setup.py
+++ Setup.py
@@ -9,7 +9,7 @@ version = "1.12.60"
 packages = [package for package in find_packages() if not package.endswith("Tests")]
 
 folder_of_current_file = os.path.dirname(os.path.realpath(__file__))
-with open(str(Path(os.path.join(folder_of_current_file, f"ReadMe.md")).resolve()), "r", encoding='utf-8') as file:
+with open(os.path.join(folder_of_current_file, f"ReadMe.md"), "r", encoding='utf-8') as file:
     long_description = file.read()
 
 setup(
