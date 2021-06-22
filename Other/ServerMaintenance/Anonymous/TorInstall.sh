70f8d715334a73175e12dbef7105ca1d884498cf
Marius Göcke <marius.goecke@gmail.com>
2021-06-23 00:07:45 +0200

    little updates in scripts

--- Other/ServerMaintenance/Anonymous/TorInstall.sh
+++ Other/ServerMaintenance/Anonymous/TorInstall.sh
@@ -7,6 +7,5 @@
 echo "deb http://deb.torproject.org/torproject.org $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/tor.list
 pushd $(dirname $0)
 ../Common/AptUpdate.sh
-../Anonymous/TorInstall.sh
-apt-get -y install tor=0.3.5.12-1
+apt-get -y install tor
 pushd $(dirname $0)
--- Other/ServerMaintenance/Common/Hardening.sh
+++ Other/ServerMaintenance/Common/Hardening.sh
@@ -5,6 +5,7 @@ pushd $(dirname $0)
 # TODO
 # deinstall/disable sudo, wget, ping, git, find, chown, chmod, etc.
 # disable root-login
+# create user without root-privileges
 # shrink rights of all user as much as possible
 # etc.
 popd
