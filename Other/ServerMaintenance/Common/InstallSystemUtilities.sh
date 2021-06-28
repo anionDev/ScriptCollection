aeda6414a009772166bf02a1a90764e24df7ae7c
Marius Göcke <marius.goecke@gmail.com>
2021-06-28 21:19:32 +0200

    added installsystemutilities

---
+++ Other/ServerMaintenance/Common/InstallSystemUtilities.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# This script is intended to be executed inside a container as user with root privileges.
+pushd $(dirname $0)
+../AptUpdate.sh
+apt-get -y install gpg sudo wget curl net-tools iputils-ping tree procps
+popd
