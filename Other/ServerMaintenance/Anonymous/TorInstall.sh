b3ccbe7cebebc5739219c3d082cf13e9952a9f4d
Marius Göcke <marius.goecke@gmail.com>
2021-07-05 00:39:06 +0200

    added missing gpg-import

--- Other/ServerMaintenance/Anonymous/TorInstall.sh
+++ Other/ServerMaintenance/Anonymous/TorInstall.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# This script is intended to be executed as root-user.
+# This script is intended to be executed on a debian-system as root-user.
 # Content semantically from: https://support.torproject.org/apt/tor-deb-repo/
 # After execution usually the following paths will be used by default:
 # /etc/tor (configuration)
@@ -8,10 +8,10 @@
 pushd $(dirname $0)
 
 ../Common/InstallSystemUtilities.sh
-
-echo "deb http://deb.torproject.org/torproject.org buster main" | tee -a /etc/apt/sources.list.d/tor.list
-echo "deb-src https://deb.torproject.org/torproject.org buster main" | tee -a /etc/apt/sources.list.d/tor.list
-
+echo "deb http://deb.torproject.org/torproject.org $(lsb_release -cs) main" | tee -a /etc/apt/sources.list.d/tor.list
+echo "deb-src https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | tee -a /etc/apt/sources.list.d/tor.list
+echo "" | tee -a /etc/apt/sources.list.d/tor.list
+wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
 
 apt-get -y update
