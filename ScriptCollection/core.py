8b6271ebf80a20831b1301e35c7825cac3e63e8e
Marius Göcke <marius.goecke@gmail.com>
2021-07-20 22:02:09 +0200

    fix in fix in docker_create_image_release_postmerge

--- ScriptCollection/core.py
+++ ScriptCollection/core.py
@@ -521,11 +521,11 @@ class ScriptCollection:
         if (self.get_boolean_value_from_configuration(configparser, "docker", "storeimageinartefactdirectory")):
             ensure_directory_exists(artefactdirectory)
             for tag in tags_for_export:
-                if tag != "latest":
-                    prefix = "v"
+                if tag.endswith(":latest"):
+                    separator = "_"
                 else:
-                    prefix = ""
-                targetfile_name = tag.replace(":", "_") + prefix + ".tar"
+                    separator = "_v"
+                targetfile_name = tag.replace(":", separator) +  ".tar"
                 targetfile = os.path.join(artefactdirectory, targetfile_name)
                 if os.path.isfile(targetfile):
                     if self.get_boolean_value_from_configuration(configparser, "docker", "overwriteexistingfilesinartefactdirectory"):
