432b5f6899340328eef1148cecfc2eee9822b45f
Marius Göcke <marius.goecke@gmail.com>
2021-12-14 01:55:04 +0100

    added some hardening

--- Other/ServerMaintenance/Debian/Common/Hardening.sh
+++ Other/ServerMaintenance/Debian/Common/Hardening.sh
@@ -1,62 +1,37 @@
 #!/bin/bash
 
-# This script is intended to be run as user with root privileges.
-
 #Arguments:
-applicationstokeep="$2" #semicolon-separated list
-additionalFolderToRemove="$3" #semicolon-separated list
+applicationstokeep="$1" #semicolon-separated list
+additionalfolderstoremove="$2" #semicolon-separated list
 
-IFS=';' read -ra ADDR <<< "$additionalFolderToRemove"
-for i in "${ADDR[@]}"; do
-  rm -rf "$i"
-done
+apt-get install -y python3 python3-pip
+pip3 install ScriptCollection
 
-cd /
+python3 Hardening.py --applicationstokeep "$applicationstokeep" --additionalfolderstoremove "$additionalfolderstoremove"
 
-#-----------------------------------------------------------------------------------------------
-#!/bin/bash
-
-function list_include_item {
-  local list="$1"
-  local item="$2"
-  if [[ $list =~ (^|[[:space:]])"$item"($|[[:space:]]) ]] ; then
-    # yes, list include item
-    result=0
-  else
-    result=1
-  fi
-  return $result
-}
-
-applicationstokeepasstring="openjdk-17-jre-headless
-git
-bash"
-
-SAVEIFS=$IFS   # Save current IFS
-IFS=$'\n'      # Change IFS to new line
-applicationstokeep=($applicationstokeepasstring) # split to array applicationstokeep
-IFS=$SAVEIFS   # Restore IFS
-
-applicationstodelete=("git" "bash" "curl" "wget")
-
-for (( i=0; i<${#applicationstodelete[@]}; i++ ))
-do
-    applicationtodelete=$applicationstodelete[$i]
-    if [[ " ${applicationstokeep} " =~ " ${applicationtodelete} " ]]; then
-        echo "$applicationtodelete"
-    fi
-done
-
-
-#-----------------------------------------------------------------------------------------------
-
-apt-get autoremove
+# Remove unnecessary binaries
+apt-get remove -y python-pip3 python3
+apt-get autoclean -y
+apt-get autoremove -y
 apt-get clean
-
-# TODO:
-# - deinstall/disable sudo, wget, ls, ping, git, find, chown, chmod, apt etc. and all other applications which are not listed in $applicationstokeep
-# - generally disable root-login
-# - shrink rights of all user as much as possible
-# - prevent writing files using something like "echo x > y"
-# - prevent executing files as much as possible
-# etc.
+apt-get remove -y --allow-remove-essential apt
+rm /bin/chgrp
+rm /bin/chmod
+rm /bin/chown
+rm /bin/dpkg
+rm /bin/echo
+rm /bin/grep
+rm /bin/hostname
+rm /bin/mount
+rm /bin/sh
+rm /bin/touch
+rm /bin/umount
+rm /bin/uname
+rm /bin/su
+rm /usr/bin/tree
+rm /usr/bin/truncate
+rm /usr/bin/sudo
+rm /usr/bin/xargs
+rm /bin/ls
+rm /usr/bin/find
+rm /usr/bin/which
