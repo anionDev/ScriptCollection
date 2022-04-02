f0ffea15061c0952f65ea3aee5d231ad3a973ab3
Marius Göcke <marius.goecke@gmail.com>
2022-04-02 13:55:08 +0200

    fixes

--- ScriptCollection/GenericGitRunner.py
+++ ScriptCollection/GenericGitRunner.py
@@ -10,5 +10,5 @@ class GenericGitRunner(GitRunnerBase):
         self.git_command_runner_function = git_command_runner_function
         self.custom_arguments = custom_arguments
 
-    def run_git(self, arguments_as_array: list[str], working_directory: str, throw_exception_if_exitcode_is_not_zero: bool) -> list[int, str, str, int]:
+    def run_git_argsasarray(self, arguments_as_array: list[str], working_directory: str, throw_exception_if_exitcode_is_not_zero: bool) -> list[int, str, str, int]:
         return self.git_command_runner_function(arguments_as_array, working_directory, throw_exception_if_exitcode_is_not_zero, self.custom_arguments)
