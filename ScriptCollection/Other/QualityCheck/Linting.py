fcd9b81ca60ac00394859ea5045a5a1b3e0cb8ef
Marius Göcke <marius.goecke@gmail.com>
2022-04-27 01:41:28 +0200

    fix

--- ScriptCollection/Other/QualityCheck/Linting.py
+++ ScriptCollection/Other/QualityCheck/Linting.py
@@ -1,4 +1,4 @@
-from ScriptCollection.ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
+from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
 
 def linting():
     ScriptCollectionCore().standardized_tasks_linting_for_python_project_in_common_project_structure(__file__)
