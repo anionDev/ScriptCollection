5fae0e5fe41c05f4160298e7f554b64754b94825
Marius Göcke <marius.goecke@gmail.com>
2022-04-27 01:38:31 +0200

    fix

--- ScriptCollection/Setup.py
+++ ScriptCollection/Setup.py
@@ -15,7 +15,7 @@ def create_wheel_file():
     folder_of_current_file = os.path.dirname(__file__)
     packages = [package for package in find_packages() if not package.endswith("Tests")]
 
-    with open(os.path.join(Path(folder_of_current_file).absolute(), "ReadMe.md"), "r", encoding='utf-8') as file:
+    with open(os.path.join(Path(folder_of_current_file).parent.absolute(), "ReadMe.md"), "r", encoding='utf-8') as file:
         long_description = file.read()
 
     setup(
