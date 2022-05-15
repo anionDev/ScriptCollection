f01e68193363d7f39015206259429619f84f8109
Marius Göcke <marius.goecke@gmail.com>
2022-05-15 11:24:36 +0200

    test

--- ScriptCollection/Other/QualityCheck/RunTestcases.py
+++ ScriptCollection/Other/QualityCheck/RunTestcases.py
@@ -1,9 +1,9 @@
 from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
-
+import pathlib
 
 def run_testcases():
+    test=pathlib.Path(__file__).parent.resolve()
     ScriptCollectionCore().standardized_tasks_run_testcases_for_python_project_in_common_project_structure(__file__)
 
 
-if __name__ == "__main__":
-    run_testcases()
+run_testcases()
