cf7ded0efb10b831448d06f5c1b8ef76d8b4affe
Marius Göcke <marius.goecke@gmail.com>
2022-05-29 13:38:03 +0200

    added standardized_tasks_linting_for_dotnet_project_in_common_project_structure-operation

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -546,6 +546,9 @@ class ScriptCollectionCore:
         else:
             raise ValueError(f"Version '{current_version}' does not match version-regex '{versiononlyregex}'")
 
+    def standardized_tasks_linting_for_dotnet_project_in_common_project_structure(self,linting_script_file:str, args:list[str]):
+        pass# TODO
+
     def standardized_tasks_generate_reference_by_docfx(self, generate_reference_script_file: str) -> None:
         folder_of_current_file = os.path.dirname(generate_reference_script_file)
         generated_reference_folder = os.path.join(folder_of_current_file, "GeneratedReference")
