ff7b319007872d628a5718d8ea8f47b38284a6ce
Marius Göcke <marius.goecke@gmail.com>
2022-07-19 12:12:10 +0200

    updates

--- ScriptCollection/Other/Reference/GenerateReference.py
+++ ScriptCollection/Other/Reference/GenerateReference.py
@@ -4,7 +4,7 @@ from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjec
 
 
 def generate_reference():
-    TasksForCommonProjectStructure().standardized_tasks_generate_reference_by_docfx(str(Path(__file__).absolute()), sys.argv)
+    TasksForCommonProjectStructure().standardized_tasks_generate_reference_by_docfx(str(Path(__file__).absolute()), 1, sys.argv)
 
 
 if __name__ == "__main__":
