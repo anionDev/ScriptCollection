676fe9924378ef4603bf989a107d5c7b52e48ead
Marius Göcke <marius.goecke@gmail.com>
2022-10-14 12:17:47 +0200

    little update in commontasks without semantical change

--- ScriptCollection/Other/CommonTasks.py
+++ ScriptCollection/Other/CommonTasks.py
@@ -11,8 +11,8 @@ def common_tasks():
     folder_of_current_file = os.path.dirname(file)
     sc = ScriptCollectionCore()
     tfcps = TasksForCommonProjectStructure()
-    version = sc.getversion_from_arguments_or_gitversion(file, sys.argv)
     tfcps.standardized_tasks_do_common_tasks(file, 1, "QualityCheck", sys.argv)
+    version = sc.getversion_from_arguments_or_gitversion(file, sys.argv)
     sc.replace_version_in_python_file(GeneralUtilities.resolve_relative_path("../Setup.py", folder_of_current_file), version)
     sc.replace_version_in_python_file(GeneralUtilities.resolve_relative_path("../ScriptCollection/ScriptCollectionCore.py", folder_of_current_file), version)
 
