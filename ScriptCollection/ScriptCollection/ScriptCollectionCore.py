5b29b7e170b4abeef6d65b50e809cd56f3e755df
Marius Göcke <marius.goecke@gmail.com>
2022-10-16 12:06:06 +0200

    Created v3.2.4

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -25,7 +25,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
 
-version = "3.2.3"
+version = "3.2.4"
 __version__ = version
 
 
