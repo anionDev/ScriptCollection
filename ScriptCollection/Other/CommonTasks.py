87498375fcd67c7de99f3477bce9d8540a58691d
Marius Göcke <marius.goecke@gmail.com>
2022-10-18 00:33:03 +0200

    replaced setup.py by pyproject.toml

--- ScriptCollection/Other/CommonTasks.py
+++ ScriptCollection/Other/CommonTasks.py
@@ -14,7 +14,7 @@ def common_tasks():
     sc.replace_version_in_python_file(GeneralUtilities.resolve_relative_path("../Setup.py", folder_of_current_file), version)
     sc.replace_version_in_python_file(GeneralUtilities.resolve_relative_path("../ScriptCollection/ScriptCollectionCore.py", folder_of_current_file), version)
     tfcps = TasksForCommonProjectStructure()
-    tfcps.standardized_tasks_do_common_tasks(file, 1, "QualityCheck", sys.argv, True)
+    tfcps.standardized_tasks_do_common_tasks(file, 1, "QualityCheck", True, sys.argv)
 
 
 if __name__ == "__main__":
