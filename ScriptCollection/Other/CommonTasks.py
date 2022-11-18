a057c330f032bceaa69b6010de2c5317bf3d2fea
Marius Göcke <marius.goecke@gmail.com>
2022-11-18 15:12:15 +0100

    fix; renamed buildenvironment to targetenvironmenttype

--- ScriptCollection/Other/CommonTasks.py
+++ ScriptCollection/Other/CommonTasks.py
@@ -5,15 +5,20 @@ from ScriptCollection.GeneralUtilities import GeneralUtilities
 from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
 from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjectStructure
 
+
 def common_tasks():
     file = str(Path(__file__).absolute())
     folder_of_current_file = os.path.dirname(file)
     sc = ScriptCollectionCore()
+    cmd_args = sys.argv
+    tfcps = TasksForCommonProjectStructure()
+    verbosity = tfcps.get_verbosity_from_commandline_arguments(cmd_args, 1)
+    buildenvironment = tfcps.get_buildenvironment_from_commandline_arguments(cmd_args, "QualityCheck")
+    additional_arguments_file = tfcps.get_additionalargumentsfile_from_commandline_arguments(cmd_args, None)
     version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path("../..", os.path.dirname(file)))
     sc.replace_version_in_pyproject_file(GeneralUtilities.resolve_relative_path("../pyproject.toml", folder_of_current_file), version)
     sc.replace_version_in_python_file(GeneralUtilities.resolve_relative_path("../ScriptCollection/ScriptCollectionCore.py", folder_of_current_file), version)
-    tfcps = TasksForCommonProjectStructure()
-    tfcps.standardized_tasks_do_common_tasks(file, 1, "QualityCheck", True, sys.argv)
+    tfcps.standardized_tasks_do_common_tasks(file, verbosity, buildenvironment, True, sys.argv, additional_arguments_file)
 
 
 if __name__ == "__main__":
