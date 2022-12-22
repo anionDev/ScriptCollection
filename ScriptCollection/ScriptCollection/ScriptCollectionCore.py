747f97a5182d07042db0c086e66a682064863a63
Marius Göcke <marius.goecke@gmail.com>
2022-12-22 23:47:39 +0100

    update

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -25,7 +25,7 @@ from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
 
-version = "3.3.28"
+version = "3.3.29"
 __version__ = version
 
 
