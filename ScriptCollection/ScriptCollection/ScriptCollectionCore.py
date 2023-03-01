cf936862db5592723b155ce058d890dc591309ee
Marius Göcke <marius.goecke@gmail.com>
2023-03-01 22:04:08 +0100

    update in workspace and pylintrc

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -27,7 +27,7 @@ from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
 
-version = "3.3.60"
+version = "3.3.61"
 __version__ = version
 
 
