afdd1cadaf16795943d2577f24a10e9b2b7b42e3
Marius Göcke <marius.goecke@gmail.com>
2023-10-05 18:01:30 +0200

    Updates for csproj-file-validation.

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -29,7 +29,7 @@ from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
 
-version = "3.4.34"
+version = "3.4.35"
 __version__ = version
 
 
