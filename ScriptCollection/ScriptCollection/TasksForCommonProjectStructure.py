d61117976e2d1d4271e71920e47a4486ecdb02f0
Marius Göcke <marius.goecke@gmail.com>
2023-10-22 16:31:07 +0200

    fix

--- ScriptCollection/ScriptCollection/TasksForCommonProjectStructure.py
+++ ScriptCollection/ScriptCollection/TasksForCommonProjectStructure.py
@@ -1840,17 +1840,17 @@ class TasksForCommonProjectStructure:
         oci_image_artifacts_folder = GeneralUtilities.resolve_relative_path("../../../../Artifacts/BuildResult_OCIImage", folder)
         image_filename = os.path.basename(sc.find_file_by_extension(oci_image_artifacts_folder, "tar"))
         codeunit_name = os.path.basename(GeneralUtilities.resolve_relative_path("../../../../..", folder))
-        codeunit_name_lower = codeunit_name.lower()
         if remove_old_container:
             docker_compose_file=f"{folder}/docker-compose.yml"
             container_names=[]
-            for line in GeneralUtilities.read_lines_from_file(docker_compose_file):
+            lines=GeneralUtilities.read_lines_from_file(docker_compose_file)
+            for line in lines:
                 if match := re.search("container_name:\\s*'?([^']+)'?", line):
                     container_names.append(match.group(1))
             GeneralUtilities.write_message_to_stdout(f"Ensure container of {docker_compose_file} do not exist...")
             for container_name in container_names:
                 GeneralUtilities.write_message_to_stdout(f"Ensure container of {container_name} does not exist")
-                sc.run_program("docker", f"container rm -f {codeunit_name_lower}", oci_image_artifacts_folder, verbosity=verbosity,throw_exception_if_exitcode_is_not_zero=False)
+                sc.run_program("docker", f"container rm -f {container_name}", oci_image_artifacts_folder, verbosity=0, throw_exception_if_exitcode_is_not_zero=False)
         if remove_volumes_folder:
             volumes_folder = os.path.join(folder, "Volumes")
             GeneralUtilities.write_message_to_stdout(f"Ensure volumes-folder '{volumes_folder}' does not exist...")
