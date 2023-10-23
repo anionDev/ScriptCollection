54f8dae3f36d66a2072bf66340b34e6245725356
Marius Göcke <marius.goecke@gmail.com>
2023-10-23 23:50:44 +0200

    overhead

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -27,7 +27,7 @@ from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
 
-version = "3.4.38"
+version = "3.4.39"
 __version__ = version
 
 
