348d80fe0709cae7beea28d95c1f8bf543a2e50d
Marius Göcke <marius.goecke@gmail.com>
2023-11-04 09:38:29 +0100

    Updated dependencies

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -27,7 +27,7 @@ from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
 
-version = "3.4.40"
+version = "3.4.41"
 __version__ = version
 
 
