e9aaefbe34b51c9cc45b1344050ca7cba4fe0a76
Marius Göcke <marius.goecke@gmail.com>
2023-11-29 00:12:03 +0100

    update in push_docker_build_artifact

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -27,7 +27,7 @@ from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
 
-version = "3.4.48"
+version = "3.4.49"
 __version__ = version
 
 
