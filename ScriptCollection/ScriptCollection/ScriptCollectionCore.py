24faf2e5c53de4913927ffd9decbd0899e2c24bf
Marius Göcke <marius.goecke@gmail.com>
2024-01-29 23:59:28 +0100

    Fix for buildtargetenvironment for .NET-build

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -27,7 +27,7 @@ from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
 
-version = "3.4.53"
+version = "3.4.54"
 __version__ = version
 
 
