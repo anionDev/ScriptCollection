aaedb7f754f49d86b050219c37df4c6d319ef137
Marius Göcke <marius.goecke@gmail.com>
2024-05-14 01:03:24 +0200

    updates

--- ScriptCollection/ScriptCollection/ProgramRunnerBase.py
+++ ScriptCollection/ScriptCollection/ProgramRunnerBase.py
@@ -8,7 +8,7 @@ class ProgramRunnerBase:
     # Return-values program_runner: Pid
     @abstractmethod
     @GeneralUtilities.check_arguments
-    def run_program_argsasarray_async_helper(self, program: str, arguments_as_array: list[str] = [], working_directory: str = None, custom_argument: object = None) -> Popen:
+    def run_program_argsasarray_async_helper(self, program: str, arguments_as_array: list[str] = [], working_directory: str = None, custom_argument: object = None, interactive:bool=False) -> Popen:
         raise NotImplementedError
 
     # Return-values program_runner: Exitcode, StdOut, StdErr, Pid
@@ -20,23 +20,23 @@ class ProgramRunnerBase:
     # Return-values program_runner: Exitcode, StdOut, StdErr, Pid
     @abstractmethod
     @GeneralUtilities.check_arguments
-    def run_program_argsasarray(self, program: str, arguments_as_array: list[str] = [], working_directory: str = None, custom_argument: object = None) -> tuple[int, str, str, int]:
+    def run_program_argsasarray(self, program: str, arguments_as_array: list[str] = [], working_directory: str = None, custom_argument: object = None, interactive:bool=False) -> tuple[int, str, str, int]:
         raise NotImplementedError
 
     # Return-values program_runner: Exitcode, StdOut, StdErr, Pid
     @abstractmethod
     @GeneralUtilities.check_arguments
-    def run_program(self, program: str, arguments:  str = "", working_directory: str = None, custom_argument: object = None) -> tuple[int, str, str, int]:
+    def run_program(self, program: str, arguments:  str = "", working_directory: str = None, custom_argument: object = None, interactive:bool=False) -> tuple[int, str, str, int]:
         raise NotImplementedError
 
     # Return-values program_runner: Pid
     @abstractmethod
     @GeneralUtilities.check_arguments
-    def run_program_argsasarray_async(self, program: str, arguments_as_array: list[str] = [], working_directory: str = None, custom_argument: object = None) -> int:
+    def run_program_argsasarray_async(self, program: str, arguments_as_array: list[str] = [], working_directory: str = None, custom_argument: object = None, interactive:bool=False) -> int:
         raise NotImplementedError
 
     # Return-values program_runner: Pid
     @abstractmethod
     @GeneralUtilities.check_arguments
-    def run_program_async(self, program: str, arguments: str,  working_directory: str, custom_argument: object) -> int:
+    def run_program_async(self, program: str, arguments: str,  working_directory: str, custom_argument: object, interactive:bool=False) -> int:
         raise NotImplementedError
