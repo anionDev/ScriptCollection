f67bdc0cbd240c4e763923be1445ad031940dac9
Marius Göcke <marius.goecke@gmail.com>
2024-05-15 00:39:14 +0200

    udpated version

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -28,7 +28,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
-version = "3.5.2"
+version = "3.5.3"
 __version__ = version
 
 
