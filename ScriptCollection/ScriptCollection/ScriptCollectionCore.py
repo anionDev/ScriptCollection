bf66cc96b43e34f2bce302f3722abd29d8185d00
Marius Göcke <marius.goecke@gmail.com>
2024-10-01 00:29:07 +0200

    added generate_api_client_from_dependent_codeunit_in_dotnet

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -30,7 +30,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
-version = "3.5.17"
+version = "3.5.18"
 __version__ = version
 
 
