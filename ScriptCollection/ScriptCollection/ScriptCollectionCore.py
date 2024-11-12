74cfb1f530ccaeb03b76760d67ccf8d5248d7ce3
Marius Göcke <marius.goecke@gmail.com>
2024-11-12 21:19:42 +0100

    improvement in do_npm_install

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -31,7 +31,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
-version = "3.5.28"
+version = "3.5.29"
 __version__ = version
 
 
