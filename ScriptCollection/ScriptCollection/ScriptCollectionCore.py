6c707badc0ac8f1769e7a21beb318b57408be48f
Marius Göcke <marius.goecke@gmail.com>
2024-11-25 12:58:15 +0100

    added create_changelog_entry

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -31,7 +31,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
-version = "3.5.31"
+version = "3.5.32"
 __version__ = version
 
 
