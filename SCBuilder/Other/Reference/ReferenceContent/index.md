ee2333a52ded2396b8e747c1e984a114cd64c197
Marius Göcke <marius.goecke@gmail.com>
2024-12-12 23:41:37 +0100

    removed scbuilder, added taskfile

--- SCBuilder/Other/Reference/ReferenceContent/index.md
+++
@@ -1,3 +0,0 @@
-# SCBuilder reference
-
-TODO
--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -31,7 +31,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
-version = "3.5.34"
+version = "3.5.35"
 __version__ = version
 
 
