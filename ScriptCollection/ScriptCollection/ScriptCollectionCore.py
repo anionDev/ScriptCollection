947b2f170bfdcc9fe8872594ed1fd664626a83ee
Marius Göcke <marius.goecke@gmail.com>
2025-03-15 13:47:19 +0100

    fix

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -33,7 +33,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
-version = "3.5.95"
+version = "3.5.96"
 __version__ = version
 
 
