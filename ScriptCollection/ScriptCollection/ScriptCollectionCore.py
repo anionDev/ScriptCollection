cabcf1ff370c0b9dcac251fbf8cc1e1b687caf4b
Marius Göcke <marius.goecke@gmail.com>
2025-05-13 19:54:11 +0200

    added changelog

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -33,7 +33,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 
-version = "3.5.114"
+version = "3.5.115"
 __version__ = version
 
 
