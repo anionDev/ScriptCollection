a4fd416d5e1ac9e9695c538974738125b67912c7
Marius Göcke <marius.goecke@gmail.com>
2025-05-25 22:59:10 +0200

    no change

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -33,7 +33,7 @@ from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 from .SCLog import SCLog, LogLevel
 
-version = "3.5.132"
+version = "3.5.133"
 __version__ = version
 
 
