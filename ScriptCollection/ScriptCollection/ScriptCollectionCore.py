d6ee21b852dc60a6300a0d87644cbd911e0bd0eb
Marius Göcke <marius.goecke@gmail.com>
2025-07-25 16:24:15 +0200

    Fix in `SCLog`.

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -33,7 +33,7 @@ from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 from .SCLog import SCLog, LogLevel
 
-version = "3.5.147"
+version = "3.5.148"
 __version__ = version
 
 
