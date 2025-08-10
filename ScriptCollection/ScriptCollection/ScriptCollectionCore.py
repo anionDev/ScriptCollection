a7689479423f491a24295407f2511d4fee2cab6d
Marius Göcke <marius.goecke@gmail.com>
2025-08-10 14:07:53 +0200

    ignored git-ignored-files by default in t4_transform

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -33,7 +33,7 @@ from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 from .SCLog import SCLog, LogLevel
 
-version = "3.5.148"
+version = "3.5.149"
 __version__ = version
 
 
