1f6f8ab50bc61ca71412da231ba5aaaff97f6eb1
Marius Göcke <marius.goecke@gmail.com>
2025-09-20 23:01:39 +0200

    added hints for the case when csproj regex does not match

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -36,7 +36,7 @@ from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 from .SCLog import SCLog, LogLevel
 
-version = "3.5.163"
+version = "3.5.164"
 __version__ = version
 
 
