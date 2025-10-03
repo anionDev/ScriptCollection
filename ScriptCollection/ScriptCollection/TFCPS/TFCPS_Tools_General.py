ca0b915825c2d2c27fe85401c771b1755451196e
Marius Göcke <marius.goecke@gmail.com>
2025-10-03 18:59:02 +0200

    added requirement of having cyclone-dx as commandline-tool

--- ScriptCollection/ScriptCollection/TFCPS/TFCPS_Tools_General.py
+++ ScriptCollection/ScriptCollection/TFCPS/TFCPS_Tools_General.py
@@ -34,15 +34,15 @@ class TFCPS_Tools_General:
 
     @GeneralUtilities.check_arguments
     def ensure_cyclonedxcli_is_available(self, target_folder: str,enforce_update:bool) -> None:
-        local_filename = "cyclonedx-cli"
-        filename_on_github: str
-        if GeneralUtilities.current_system_is_windows():
-            filename_on_github = "cyclonedx-win-x64.exe"
-            local_filename = local_filename+".exe"
-        else:
-            filename_on_github = "cyclonedx-linux-x64"
-        self.ensure_file_from_github_assets_is_available_with_retry(target_folder, "CycloneDX", "cyclonedx-cli", "CycloneDXCLI", local_filename, lambda latest_version: filename_on_github,enforce_update=enforce_update)
-
+        if shutil.which("cyclonedx-cli") is None:
+            local_filename = "cyclonedx-cli"
+            filename_on_github: str
+            if GeneralUtilities.current_system_is_windows():
+                filename_on_github = "cyclonedx-win-x64.exe"
+                local_filename = local_filename+".exe"
+            else:
+                filename_on_github = "cyclonedx-linux-x64"
+            self.ensure_file_from_github_assets_is_available_with_retry(target_folder, "CycloneDX", "cyclonedx-cli", "CycloneDXCLI", local_filename, lambda latest_version: filename_on_github,enforce_update=False)
     @GeneralUtilities.check_arguments
     def ensure_file_from_github_assets_is_available_with_retry(self, target_folder: str, githubuser: str, githubprojectname: str, resource_name: str, local_filename: str, get_filename_on_github, amount_of_attempts: int = 5,enforce_update:bool=False) -> None:
         GeneralUtilities.retry_action(lambda: self.ensure_file_from_github_assets_is_available(target_folder, githubuser, githubprojectname, resource_name, local_filename, get_filename_on_github,enforce_update), amount_of_attempts)
@@ -453,9 +453,9 @@ class TFCPS_Tools_General:
         os.rename(os.path.join(repository_folder, target_sbom_file_relative), os.path.join(repository_folder, target_original_sbom_file_relative))
 
         self.ensure_cyclonedxcli_is_available(repository_folder,not use_cache)
-        cyclonedx_exe = os.path.join(repository_folder, "Other/Resources/CycloneDXCLI/cyclonedx-cli")
-        if GeneralUtilities.current_system_is_windows():
-            cyclonedx_exe = cyclonedx_exe+".exe"
+        cyclonedx_exe ="cyclonedx-cli"# os.path.join(repository_folder, "Other/Resources/CycloneDXCLI/cyclonedx-cli")
+        #if GeneralUtilities.current_system_is_windows():
+        #    cyclonedx_exe = cyclonedx_exe+".exe"
         self.__sc.run_program(cyclonedx_exe, f"merge --input-files {source_sbom_file_relative} {target_original_sbom_file_relative} --output-file {target_sbom_file_relative}", repository_folder)
         GeneralUtilities.ensure_file_does_not_exist(os.path.join(repository_folder, target_original_sbom_file_relative))
         self.__sc.format_xml_file(os.path.join(repository_folder, target_sbom_file_relative))
