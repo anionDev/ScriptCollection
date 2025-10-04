b41f4f20bf64686c63501c986f8229c2979856e5
Marius Göcke <marius.goecke@gmail.com>
2025-10-05 00:54:28 +0200

    Added functions for docker codeunits.

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -37,7 +37,7 @@ from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 from .SCLog import SCLog, LogLevel
 
-version = "4.0.38"
+version = "4.0.39"
 __version__ = version
 
 
