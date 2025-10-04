e955d8a4877b5d321629f25b761eb62c3fb464c7
Marius Göcke <marius.goecke@gmail.com>
2025-10-04 23:00:33 +0200

    fix

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -36,7 +36,7 @@ from .ProgramRunnerPopen import ProgramRunnerPopen
 from .ProgramRunnerEpew import ProgramRunnerEpew, CustomEpewArgument
 from .SCLog import SCLog, LogLevel
 
-version = "4.0.33"
+version = "4.0.34"
 __version__ = version
 
 
