84d08625621ba4a7b25d4bf1b09fb37d083f0b7d
Marius Göcke <marius.goecke@gmail.com>
2025-10-13 23:02:24 +0200

    removed t4-usage from TFCPS_CodeUnitSpecific_DotNet_Functions

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -36,7 +36,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.0.52"
+version = "4.0.53"
 __version__ = version
 
 
