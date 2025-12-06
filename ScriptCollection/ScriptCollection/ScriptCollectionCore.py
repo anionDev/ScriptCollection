0ba0d7e8684aa33031fea653eecde9844eb37d02
Marius Göcke <marius.goecke@gmail.com>
2025-12-06 13:25:58 +0100

    added changelog

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -35,7 +35,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.0.84"
+version = "4.0.85"
 __version__ = version
 
 
