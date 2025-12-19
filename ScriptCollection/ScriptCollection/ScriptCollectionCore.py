e0562c2d2490c8dbfca027d68c7ed0714b5ea2f4
Marius Göcke <marius.goecke@gmail.com>
2025-12-19 22:21:35 +0100

    fix

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -35,7 +35,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.0.96"
+version = "4.0.97"
 __version__ = version
 
 
