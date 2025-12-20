514d1ca3e6b7833814675f34339bfbe25e97f2a9
Marius Göcke <marius.goecke@gmail.com>
2025-12-20 18:05:19 +0100

    fx, Added possibility to add custom check for built docker image.

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -35,7 +35,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.1.3"
+version = "4.1.4"
 __version__ = version
 
 
