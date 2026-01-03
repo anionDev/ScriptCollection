c8e532212e1519b2dfceb3e784b2a94a07001208
Marius Göcke <marius.goecke@gmail.com>
2026-01-03 15:47:42 +0100

    Added get_only_item_from_list.

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -35,7 +35,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.2.18"
+version = "4.2.19"
 __version__ = version
 
 
