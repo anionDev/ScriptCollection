38b85e90fbe61da03ebe4b7902ae7ae2f8ab74b8
Marius Göcke <marius.goecke@gmail.com>
2026-01-04 20:25:34 +0100

    update

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -35,7 +35,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.2.23"
+version = "4.2.24"
 __version__ = version
 
 
