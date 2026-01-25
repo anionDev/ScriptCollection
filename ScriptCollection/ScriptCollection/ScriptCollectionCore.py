c100399f918193570081c26624e795d09a019b41
Marius Göcke <marius.goecke@gmail.com>
2026-01-25 18:38:14 +0100

    Updated dependencies

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -35,7 +35,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.2.30"
+version = "4.2.31"
 __version__ = version
 
 
