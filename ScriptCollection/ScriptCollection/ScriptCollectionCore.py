890c3c4da935f74f9a089fc4d110173b40591f00
Marius Göcke <marius.goecke@gmail.com>
2026-01-31 14:27:26 +0100

    Pre-merge-commit

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -35,7 +35,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.2.33"
+version = "4.2.34"
 __version__ = version
 
 
