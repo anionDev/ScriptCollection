b04e3148b494a2f7edc177e70c2a7f881dcf27e9
Marius Göcke <marius.goecke@gmail.com>
2026-02-22 21:32:55 +0100

    update for culturechooser

--- ScriptCollection/ScriptCollection/Resources/CultureChooser/CultureChooser.js
+++ ScriptCollection/ScriptCollection/Resources/CultureChooser/CultureChooser.js
@@ -1,27 +1,18 @@
 function getBrowserCulture() {
+    //returns a culture in the format en or en-US
     let lang = navigator.language || navigator.userLanguage || "en";
     const match = lang.match(/^[a-zA-Z]{2}(?:-[a-zA-Z]{2})?/);
     return match ? match[0] : "en";
 }
-function redirectToCulture() {
-    if (window.location.pathname !== "/") {
-        return;
-    }
 
+function redirectToCulture() {
     const supportedCultures = [/*__supportedCultures__*/];
     let culture = getBrowserCulture();
-
     if (supportedCultures.includes(culture)) {
-        window.location.replace(`/${culture}/`);
-        return;
+        window.location.replace(`/${culture}/index.html`);
+    } else {
+        throw new Error(`Unsupported culture: "${culture}"`);
     }
-
-    const baseLang = culture.split("-")[0];
-    if (supportedCultures.includes(baseLang)) {
-        window.location.replace(`/${baseLang}/`);
-        return;
-    }
-
-    window.location.replace("/en/");
 }
+
 redirectToCulture();
