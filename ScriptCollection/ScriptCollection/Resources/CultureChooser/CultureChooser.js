f7a656c462740f3ccc902410c578b38bd77c8d47
Marius Göcke <marius.goecke@gmail.com>
2026-02-23 19:55:59 +0100

    update in culturechooser.js

--- ScriptCollection/ScriptCollection/Resources/CultureChooser/CultureChooser.js
+++ ScriptCollection/ScriptCollection/Resources/CultureChooser/CultureChooser.js
@@ -1,18 +1,29 @@
 function getBrowserCulture() {
-    //returns a culture in the format en or en-US
+    //returns a culture in the format "en", "fr" and "fr-FR".
     let lang = navigator.language || navigator.userLanguage || "en";
     const match = lang.match(/^[a-zA-Z]{2}(?:-[a-zA-Z]{2})?/);
     return match ? match[0] : "en";
 }
 
-function redirectToCulture() {
-    const supportedCultures = [/*__supportedCultures__*/];
+function getCultureToRedirect() {
+    const supportedCultures = [/*__supportedCultures__*/];//contains values like "en", "fr" and "fr-FR".
     let culture = getBrowserCulture();
     if (supportedCultures.includes(culture)) {
         window.location.replace(`/${culture}/index.html`);
     } else {
-        throw new Error(`Unsupported culture: "${culture}"`);
+        if (0 < culture.indexOf('-')) {
+            languageOnly = culture.split('-')[0];
+            if (supportedCultures.includes(languageOnly)) {
+                return languageOnly;
+            }
+        }
     }
+    return "en";
+}
+
+function redirectToCulture() {
+    const culture = getCultureToRedirect();
+    window.location.replace(`/${culture}/index.html`);
 }
 
 redirectToCulture();
