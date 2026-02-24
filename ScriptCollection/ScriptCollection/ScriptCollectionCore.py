19d5e70dc6da72c1ff1dd5b0f9eba9d874a36ff2
Marius Göcke <marius.goecke@gmail.com>
2026-02-24 21:52:30 +0100

    added chnagelog

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -35,7 +35,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.2.41"
+version = "4.2.42"
 __version__ = version
 
 
