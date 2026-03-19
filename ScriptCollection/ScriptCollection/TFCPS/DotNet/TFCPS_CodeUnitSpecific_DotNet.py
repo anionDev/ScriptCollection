32888d0d6f2a44c6e338cca0d91a0e8d16a41e3c
Marius Göcke <marius.goecke@gmail.com>
2026-03-19 18:24:05 +0100

    Improved resilience for .NET-test-runner.

--- ScriptCollection/ScriptCollection/TFCPS/DotNet/TFCPS_CodeUnitSpecific_DotNet.py
+++ ScriptCollection/ScriptCollection/TFCPS/DotNet/TFCPS_CodeUnitSpecific_DotNet.py
@@ -425,7 +425,7 @@ class TFCPS_CodeUnitSpecific_DotNet_Functions(TFCPS_CodeUnitSpecific_Base):
         if os.path.isfile(os.path.join(codeunit_folder, runsettings_file)):
             arg = f"{arg} --settings {runsettings_file}"
         arg = f"{arg} /p:CollectCoverage=true /p:CoverletOutput=../Other/Artifacts/TestCoverage/Testcoverage /p:CoverletOutputFormat=cobertura"
-        self._protected_sc.run_program("dotnet", arg, codeunit_folder, print_live_output=self.get_verbosity()==LogLevel.Debug)
+        self._protected_sc.run_with_epew_with_retry("dotnet", arg, codeunit_folder, print_live_output=self.get_verbosity()==LogLevel.Debug,timeoutInSeconds=600*20)
         target_file = os.path.join(coverage_file_folder,  "TestCoverage.xml")
         GeneralUtilities.ensure_file_does_not_exist(target_file)
         os.rename(os.path.join(coverage_file_folder,  "Testcoverage.cobertura.xml"), target_file)
